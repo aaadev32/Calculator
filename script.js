e84277c9e5bb770a715aa0398c1b4a7d543b8781
rusterz <aaadev32@gmail.com>
2022-03-17 16:27:55 -0700

    added clear screen functionality

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const screen = document.getElementById('calculator-screen');
 const buttons = document.querySelectorAll('button');
+const operatorButtons = document.getElementsByClassName('operator-buttons');
+const operandButtons = document.getElementsByClassName('operand-buttons');
+const clearButton = document.getElementById('clear-button');
+let calculation = {};
 //displayValue is global to carry the value for screenDisplay until displaying output or clearing screen
 let displayValue = null;
 
@@ -59,7 +63,7 @@ function operate(operator, num1, num2) {
 
 }
 
-function screenDisplay(num) {
+function populateDisplay(num) {
 
   displayValue = document.getElementById('calculator-screen').textContent += num;
 
@@ -67,13 +71,41 @@ function screenDisplay(num) {
 
 }
 
+function clearScreen(){
+	let result = '';
+	populateDisplay(result);
+}
+
 Array.from(buttons).forEach(button => {
   button.addEventListener('click', () => {
-    let event = null;
-    event = button.textContent;
-    screenDisplay(event);
-    console.log(event);
-  });
+	let event = null;
+	let selected = button;
+	let temp = null;
+   
+	event = button.textContent;
+	temp = event;
+	
+	if(temp != null){
+		temp.concat(event);
+	}
+	
+    populateDisplay(event);
+	console.log(temp);//its almost as if i cannot concatenate temp because the script "forgets" after executing once. figure out tomorrow 3/17/22
+	
+	if(button == operatorButtons){
+		
+		calculation.num1 = parseInt(temp)
+		//add num1 to calculation object
+		temp = '';
+		//i need to make this create a num2 variable somehow to distinguish between the users operands
+		
+	}else if (selected == clearButton){
+		
+		clearScreen();
+		
+	}
+	
+	});
  
 });
-console.log(buttons);
+
